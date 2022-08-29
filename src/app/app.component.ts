c40754aca7ed4400a362807950809cdaef1e31ec
Miguel Aurela C_ <miguelaurela2804@gmail.com>
2022-08-29 08:44:03 -0500

    subirrr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipesApp';
+  nombre :string='ferNando HerrERa';
+  valor:number=1000;
+  obj={
+    Nombre:'fernando'
+  }
+  mostrarNombre(){
+    console.log(this.nombre);
+    console.log(this.valor);
+    console.log(this.obj);
+    
+    
+  }
 }
